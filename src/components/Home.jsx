cae6844fa960f0454f8d0435eac09c504da3a569
pavloskii <pavlo.igor@gmail.com>
2022-11-09 20:12:38 +0100

    filtering the users

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,27 @@ import { Loader } from "./Loader";
 import { UserCard } from "./UserCard";
 import "./Home.css";
 import { useUsers } from "../api/users";
+import { useState } from "react";
 
 export function Home() {
   const { data: users, error, isLoading } = useUsers();
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  function filterUsers(event) {
+    const value = event.target.value.toLowerCase();
+
+    const filtered = users.filter(
+      (user) =>
+        user.first_name.toLowerCase().includes(value) ||
+        user.last_name.toLowerCase().includes(value)
+    );
+
+    setFilteredUsers(filtered);
+  }
 
   if (isLoading) {
     return (
-      <div className="center-horizontally">
+      <div className="Home">
         <Loader />
       </div>
     );
@@ -19,8 +33,15 @@ export function Home() {
   }
 
   return (
-    <div className="center-horizontally">
-      {users.map((user) => (
+    <div className="Home">
+      <div className="search-container">
+        <input type="text" placeholder="Search.." onInput={filterUsers} />
+        <button>
+          <i className="fa fa-search"></i>
+        </button>
+      </div>
+
+      {filteredUsers.map((user) => (
         <UserCard
           key={user.id}
           firstName={user.first_name}

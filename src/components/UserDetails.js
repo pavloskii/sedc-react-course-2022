c34a28dab958b57777c936d7441f9a812caef161
pavloskii <pavlo.igor@gmail.com>
2022-11-09 21:06:33 +0100

    added loader to user details page

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,11 +1,18 @@
 import { useUser } from "../api/users";
+import { Loader } from "./Loader";
 
 export function UserDetails() {
-  const { data } = useUser(10);
+  const { data, isLoading } = useUser(10);
 
+  if (isLoading) {
+    return <Loader />;
+  }
+  
   return (
     <div>
-      <h1>User Details: {data.first_name} {data.last_name}</h1>
+      <h1>
+        User Details: {data.first_name} {data.last_name}
+      </h1>
     </div>
   );
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 export function useFetch(url) {
   const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
